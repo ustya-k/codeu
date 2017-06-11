69e465d98e042bf4a54291bfcbf8aa9acaeb5d83
ustya-k <ustinia98@gmail.com>
2017-06-11 16:49:59 +0300

    comments added

--- week3/Q1.py
+++ week3/Q1.py
@@ -1,4 +1,15 @@
 def get_graph(values, n, m):
+	'''
+	Builds a graph.
+
+	Args:
+		values: str, elements of the table, all rows in one line
+		n: int, number of columns
+		m: int, number of rows
+
+	Returns:
+		dict, includes two elements with keys 'nodes' (stores a dict where key is number of a node and value is node's value) and 'vertices' (stores a dict where key is a number of a node and value is a set of numbers of nodes to which it is connected)
+	'''
 	nodes = {i: values[i] for i in range(n*m)}
 	vertices = []
 	for i in range(n*m):
@@ -45,6 +56,15 @@ def get_graph(values, n, m):
 
 
 def _get_prefixes_list(dictionary):
+	'''
+	Gets list of all possible prefixes in dictionary.
+
+	Args:
+		dictionary: set
+
+	Returns:
+		set, all possible prefixes
+	'''
 	prefixes = set()
 	for word in dictionary:
 		for i, letter in enumerate(word):
@@ -53,6 +73,16 @@ def _get_prefixes_list(dictionary):
 
 
 def _isWord(line, dictionary):
+	'''
+	Checks if sequence of elements is a real word.
+
+	Args:
+		line: str, sequence of elements
+		dictionary: set
+
+	Returns:
+		boolean
+	'''
 	if line in dictionary:
 		return True
 	else:
@@ -60,6 +90,16 @@ def _isWord(line, dictionary):
 
 
 def _isPrefix(line, prefixes):
+	'''
+	Checks if sequence of elements is a possible prefix.
+
+	Args:
+		line: str, sequence of elements
+		prefixes: set
+
+	Returns:
+		boolean
+	'''
 	if line in prefixes:
 		return True
 	else:
@@ -67,6 +107,20 @@ def _isPrefix(line, prefixes):
 
 
 def _walk_table(line, node, table, dictionary, prefixes, visited_nodes):
+	'''
+	Walks through the table, looking for words.
+
+	Args:
+		line: str, sequence of elements from visited nodes
+		node: int, number of the last visited node
+		table: dict
+		dictionary: set
+		prefixes: set
+		visited_nodes: set of int, nodes that had been visited and can't be used again
+
+	Returns:
+		set, words from dictionary that can be build from the chosen node
+	'''
 	words = set()
 	if _isPrefix(line, prefixes):
 		if _isWord(line, dictionary):
@@ -80,6 +134,16 @@ def _walk_table(line, node, table, dictionary, prefixes, visited_nodes):
 
 
 def get_words_in_table(dictionary, table):
+	'''
+	Gets all real words from the table.
+
+	Args:
+		dictionary: set
+		table: set
+
+	Returns:
+		set, all real words in the table 
+	'''
 	words = set()
 	prefixes = _get_prefixes_list(dictionary)
 	for node in table['nodes']:

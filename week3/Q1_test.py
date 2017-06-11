de88b8d497e2ecd433ce31f6925ca0fbd1b00695
ustya-k <ustinia98@gmail.com>
2017-06-11 16:25:33 +0300

    some tests added

--- week3/Q1_test.py
+++ week3/Q1_test.py
@@ -2,20 +2,31 @@ import Q1
 import unittest
 
 
-class WordSearchTest(unittest.TestCase):
+class WordSearchTest(unittest.TestCase):		
 
-	def setUp(self):
-		self.dictionary = {'CAR', 'CARD', 'CART', 'CAT'}
+	def testBaseCase(self):
+		self.dictionary = {'CAR', 'CARD', 'CART', 'CAT', 'RAR'}
 		columns = 3
 		rows = 2
 		lines = 'AARTCD'
 		self.field = Q1.get_graph(lines, columns, rows)
-		
-
-	def testBaseCases(self):
 		self.assertEqual({'CAR', 'CARD', 'CAT'}, Q1.get_words_in_table(self.dictionary, self.field))
-		#self.assertEqual(Q2.get_common_ancestor(self.binary_tree, 5, 14), Q2.get_common_ancestor(self.binary_tree, 14, 5))
-		#self.assertEqual(16, Q2.get_common_ancestor(self.binary_tree, 9, 16))
+
+	def testSingleRowCase(self):
+		self.dictionary = {'AB', 'CA'}
+		columns = 4
+		rows = 1
+		lines = 'ABCD'
+		self.field = Q1.get_graph(lines, columns, rows)
+		self.assertEqual({'AB'}, Q1.get_words_in_table(self.dictionary, self.field))
+
+	def testOneCellCase(self):
+		self.dictionary = {'A', 'B'}
+		columns = 1
+		rows = 1
+		lines = 'A'
+		self.field = Q1.get_graph(lines, columns, rows)
+		self.assertEqual({'A'}, Q1.get_words_in_table(self.dictionary, self.field))
 
  
 if __name__ == '__main__':
